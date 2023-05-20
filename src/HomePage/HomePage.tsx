df5660069d5b1ec0a7155221250d1bdf99651393
Raymond Shiner <raymondshiner@gmail.com>
2023-05-20 15:15:02 -0700

    remove unsued code

--- src/HomePage/HomePage.tsx
+++ src/HomePage/HomePage.tsx
@@ -1,14 +1,10 @@
-import { useTheme } from "@emotion/react";
 import { NewPlaylistButton } from "./NewPlaylistButton";
 import { Button } from "@mui/material";
 
 export const HomePage = () => {
-    const theme = useTheme();
-    console.log({ theme });
-
     return (
         <>
-            <Button color="secondary">Home</Button>
+            <Button>Home</Button>
             <Button>Search</Button>
             <Button>Your Library</Button>
             <Button>Playlists</Button>
