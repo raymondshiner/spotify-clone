1f1558759440341a6573353054b8c497d4689230
Raymond Shiner <raymondshiner@gmail.com>
2023-05-20 14:16:25 -0700

    render library section main navigation

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,14 @@ function App() {
     <>
       <button>Home</button>
       <button>Search</button>
+      <button>Your Library</button>
+      <button>Playlists</button>
+      <button>Podcasts & Shows</button>
+      <button>Albums</button>
+      <button>Artists</button>
+      <button>Downloaded</button>
+
+      <button data-testid="add-playlist-button">Add Playlist</button>
     </>
   );
 }
--- tests/Home.spec.ts
+++ tests/Home.spec.ts
@@ -1,12 +1,28 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, Page } from "@playwright/test";
+
+const assertButtonVisible = async (page: Page, name: string) => {
+  await expect(page.getByRole("button", { name: name })).toBeVisible();
+};
 
 test.describe("Home Page", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto("/");
   });
 
-  test("Displays Main Navigation", async ({ page }) => {
-    await expect(page.getByRole("button", { name: "Home" })).toBeVisible();
-    await expect(page.getByRole("button", { name: "Search" })).toBeVisible();
+  test("displays main navigation", async ({ page }) => {
+    await assertButtonVisible(page, "Home");
+    await assertButtonVisible(page, "Search");
+  });
+
+  test("displays your library section main navigation", async ({ page }) => {
+    await assertButtonVisible(page, "Your Library");
+
+    await expect(page.getByTestId("add-playlist-button")).toBeVisible();
+
+    await assertButtonVisible(page, "Playlists");
+    await assertButtonVisible(page, "Podcasts & Shows");
+    await assertButtonVisible(page, "Albums");
+    await assertButtonVisible(page, "Artists");
+    await assertButtonVisible(page, "Downloaded");
   });
 });
